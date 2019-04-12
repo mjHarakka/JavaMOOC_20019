0d0a894109e4c3f4067d44535b6296a96525d7aa
mjHarakka <mj.harakka@gmail.com>
2019-04-12 21:35:33 +0300

    12_08 done

--- osa12-Osa12_08.KayttajanOtsikko/src/main/java/otsikko/Main.java
+++ osa12-Osa12_08.KayttajanOtsikko/src/main/java/otsikko/Main.java
@@ -1,11 +1,19 @@
 package otsikko;
 
+import java.util.Scanner;
+import javafx.application.Application;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner lukija = new Scanner(System.in);
+        
+        System.out.println("Anna otsikko sovellukselle: ");
+        String otsikko = lukija.nextLine();
+           
+        Application.launch(KayttajanOtsikko.class, "--otsikko=" + otsikko);
     }
 
 }
+
