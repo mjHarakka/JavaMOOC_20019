092eb8509fd3954cca5b39b3f8450594c7b8d13c
mjHarakka <mj.harakka@gmail.com>
2019-05-15 21:40:40 +0300

    working

---
+++ osa13-Osa13_08.Hurraa/src/main/java/hurraa/HurraaSovellus.java
@@ -0,0 +1,35 @@
+package hurraa;
+
+import static javafx.application.Application.launch;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
+import javafx.stage.Stage;
+
+public class HurraaSovellus extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane pane = new BorderPane();
+ 
+        Button nappi = new Button("Hurraa!");
+        pane.setCenter(nappi);
+ 
+        AudioClip hurraa = new AudioClip("file:Applause-Yannick_Lemieux.wav");
+        nappi.setOnAction((event) -> hurraa.play());
+ 
+        Scene scene = new Scene(pane, 600, 400);
+ 
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
